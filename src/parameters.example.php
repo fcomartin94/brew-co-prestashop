dadda57ce9e55b2078d92112c95e4cf80f8824c8
fcomartin94 <martinherzog.fa@gmail.com>
2026-06-15 23:32:24 +0200

    Add screenshots and configuration example

---
+++ src/parameters.example.php
@@ -0,0 +1,14 @@
+<?php return array (
+  'parameters' =>
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '3306',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'ps_creation_date' => '2026-06-12',
+    'locale' => 'es-ES',
+  ),
+);
